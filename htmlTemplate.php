163a357097470d446e8ec82b8cce9e19aad64143
Devin S. Combs <dscombs@hawkmail.hfcc.edu>
2025-05-03 20:09:55 -0400

    Reorganize and did resume

---
+++ htmlTemplate.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <!-- metadata for the web page -->
+     <meta charset="UTF-8">
+     <meta name="keywords" content="insert, keywords, here">
+     <meta name="description" content="Insert description here">
+     <title>Insert Title here</title>
+     <!-- insert link to CSS file if needed -->
+     
+
+</head>
+
+<body>
+     <!-- main content of web page  -->
+
+</body>
+</html>
