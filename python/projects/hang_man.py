4da0bbb01c3e7f40b6b5fc721b2d589a871e21c0
Devin Combs <dscombs@hawkmail.hfcc.edu>
2025-05-19 15:17:19 -0400

    Update to Hangman

--- python/projects/hang_man.py
+++ python/projects/hang_man.py
@@ -11,20 +11,39 @@ radnum = random.randrange(0, len(words))
 word = words[radnum]
 
 answer = ["_"] * len(word)
+hp = 5
+right = ""
+
+while hp != 0 and "_" in answer:
+    correct = False
+    print("\n\nHP: ",hp)
+    print("Guessed Letters: ",right)
+    print("Word: ", " ".join(answer))
+    print("Guess a Letter: ")
+    guess = input()
+
+    while not guess.isalpha():
+        print("\nThats not a letter, try again: ")
+        guess = input()
+
+    guess.lower()
+
+    for x in range(len(word)):
+        if guess == word[x]:
+            print("\nRight Guess!")
+            answer[x] = guess
+
+            correct = True
+
+    right += guess
+    if correct == False:
+        hp -=1
+        print("\nWrong! -1 Hp")
 
 
-print("Word: ", " ".join(answer))
-print(word,": Guess a Letter: ")
-guess = input()
+if "_" not in answer: print("\nYou Won! The Word was: ",word)
+else: print("\nYou Lost! THe Word was: ",word)
 
-for x in word:
-    if guess in word:
-        print("Right Guess!")
-        answer = guess
-        break
-
-
-print("Word: ", " ".join(answer))
 
 
 
